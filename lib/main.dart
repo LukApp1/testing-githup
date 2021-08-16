d88d8af054559f7ea635de8872022a736f480810
Faisal <you@example.com>
2021-08-16 13:52:49 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('gitHup'),
+      ),
+      body: Center(
+        child: Text('HomePage'),
+      ),
+    );
+  }
+}
